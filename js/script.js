2f68e2f1ac787995410045003068761de2608a73
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-09-16 01:55:15 +0530

    added card

--- js/script.js
+++ js/script.js
@@ -2,11 +2,43 @@ const globalState = {
   currentPage: window.location.pathname,
 };
 
-//fetch popular movie
-
+//display popular movie
 const displayPopularMovies = async () => {
-  const result = await fetchAPIData("movie/popular");
-  console.log(result);
+  const { results } = await fetchAPIData("movie/popular");
+
+  results.forEach((movie) => {
+    console.log(movie);
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `
+                <img
+                  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                  class="card-img-top"
+                  alt="Movie Title"
+                />
+              `
+                : `
+                <img
+                  src="images/no-image.jpg"
+                  class="card-img-top"
+                  alt="Movie Title"
+                />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+            </div>
+    `;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
 };
 
 //fetch API Data
